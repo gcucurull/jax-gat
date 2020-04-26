4d48e2b4c7d65e7f4ee9ece094afc48cc78a8268
Guillem Cucurull <gcucurull@gmail.com>
2020-04-26 09:23:53 +0100

    improve model definition

--- models.py
+++ models.py
@@ -1,10 +1,8 @@
-import math
 from typing import List
 
 import jax.numpy as np
 from jax import lax, random
-from jax.nn.initializers import glorot_normal, normal, uniform
-from jax.nn.initializers import glorot_uniform
+from jax.nn.initializers import glorot_normal, glorot_uniform
 import jax.nn as nn
 
 
@@ -57,6 +55,8 @@ def GraphAttentionLayer(out_dim, dropout, residual=False):
     def apply_fun(params, x, adj, rng, activation=nn.elu, is_training=False, 
                   **kwargs):
         W, a1, a2 = params
+        k1, k2, k3 = random.split(rng, 3) 
+        x = drop_fun(None, x, is_training=is_training, rng=k1)
         x = np.dot(x, W)
 
         f_1 = np.dot(x, a1) 
@@ -65,13 +65,53 @@ def GraphAttentionLayer(out_dim, dropout, residual=False):
         coefs = nn.softmax(
             nn.leaky_relu(logits, negative_slope=0.2) + np.where(adj, 0., -1e9))
 
-        k1, k2 = random.split(rng, 2) 
-        coefs = drop_fun(None, coefs, is_training=is_training, rng=k1)
-        x = drop_fun(None, x, is_training=is_training, rng=k2)
+        coefs = drop_fun(None, coefs, is_training=is_training, rng=k2)
+        x = drop_fun(None, x, is_training=is_training, rng=k3)
 
         ret = np.matmul(coefs, x)
 
         return activation(ret)
+
+    return init_fun, apply_fun
+
+
+def MultiHeadLayer(nheads: int, nhid: int, dropout: float, residual: bool=False,
+                   last_layer: bool=False):
+    layer_funs, layer_inits = [], []
+    for head_i in range(nheads):
+        att_init, att_fun = GraphAttentionLayer(nhid, dropout=dropout,
+                                residual=residual)
+        layer_inits.append(att_init)
+        layer_funs.append(att_fun)
+    
+    def init_fun(rng, input_shape):
+        params = []
+        for att_init_fun in layer_inits:
+            rng, layer_rng = random.split(rng)
+            layer_shape, param = att_init_fun(layer_rng, input_shape)
+            params.append(param)
+        input_shape = layer_shape
+        if not last_layer:
+            # multiply by the number of heads
+            input_shape = input_shape[:-1] + (input_shape[-1]*len(layer_inits),)
+        return input_shape, params
+    
+    def apply_fun(params, x, adj, is_training=False, **kwargs):
+        rng = kwargs.pop('rng', None)
+        layer_outs = []
+        assert len(params) == nheads
+        for head_i in range(nheads):
+            layer_params = params[head_i]
+            rng, _ = random.split(rng)
+            layer_outs.append(layer_funs[head_i](
+                    layer_params, x, adj, rng=rng, is_training=is_training))
+        if not last_layer:
+            x = np.concatenate(layer_outs, axis=1)
+        else:
+            # average last layer heads
+            x = np.mean(np.stack(layer_outs), axis=0)
+
+        return x
 
     return init_fun, apply_fun
 
@@ -84,65 +124,32 @@ def GAT(nheads: List[int], nhid: List[int], nclass: int, dropout: float,
 
     init_funs = []
     attn_funs = []
-    _, drop_fun = Dropout(dropout)
 
     nhid += [nclass]
     for layer_i in range(len(nhid)):
-        layer_funs, layer_inits = [], []
-        for head_i in range(nheads[layer_i]):
-            att_init, att_fun = GraphAttentionLayer(nhid[layer_i], 
-                                    dropout=dropout,
-                                    residual=residual)
-            layer_inits.append(att_init)
-            layer_funs.append(att_fun)
-        attn_funs.append(layer_funs)
-        init_funs.append(layer_inits)
+        last = layer_i == len(nhid) - 1
+        layer_init, layer_fun = MultiHeadLayer(nheads[layer_i], nhid[layer_i],
+                                    dropout=dropout, residual=residual,
+                                    last_layer=last)
+        attn_funs.append(layer_fun)
+        init_funs.append(layer_init)
 
     def init_fun(rng, input_shape):
         params = []
-        for i, layer_inits in enumerate(init_funs):
-            for init_fun in layer_inits:
-                rng, layer_rng = random.split(rng)
-                layer_shape, param = init_fun(layer_rng, input_shape)
-                params.append(param)
+        for i, init_fun in enumerate(init_funs):
+            rng, layer_rng = random.split(rng)
+            layer_shape, param = init_fun(layer_rng, input_shape)
+            params.append(param)
             input_shape = layer_shape
-            if i < len(init_funs) - 1: # not the last layer
-                # multiply by the number of heads
-                input_shape = input_shape[:-1] + (input_shape[-1]*len(layer_inits),)
         return input_shape, params
 
     def apply_fun(params, x, adj, is_training=False, **kwargs):
         rng = kwargs.pop('rng', None)
+        rngs = random.split(rng, len(attn_funs))
 
-        x = drop_fun(None, x, is_training=is_training, rng=rng)
-        rngs = random.split(rng, len(nhid)-1)
-
-        for layer_i in range(len(nhid)-1):
-            layer_outs = []
-            for head_i in range(nheads[layer_i]):
-                # TODO: this index could be wrong if each layer has a different
-                # number of heads
-                idx = layer_i * nheads[layer_i] + head_i
-                layer_params = params[idx]
-                rng, _ = random.split(rng)
-                layer_outs.append(attn_funs[layer_i][head_i](
-                        layer_params, x, adj, rng=rng, is_training=is_training))
-            x = np.concatenate(layer_outs, axis=1)
-            x = drop_fun(None, x, is_training=is_training, rng=rngs[layer_i])
+        for i, layer_fun in enumerate(attn_funs):
+            x = layer_fun(params[i], x, adj, rng=rngs[i], is_training=is_training)
         
-        # out layer
-        layer_outs = []
-        for head_i in range(nheads[-1]):
-            layer_i = len(nhid)-1
-            idx = layer_i * nheads[-2] + head_i
-            layer_params = params[idx]
-            rng, _ = random.split(rng)
-            layer_outs.append(attn_funs[layer_i][head_i](
-                    layer_params, x, adj, activation=lambda x: x, rng=rng,
-                    is_training=is_training))
-
-        # average last layer heads
-        x = np.mean(np.stack(layer_outs), axis=0) 
         return nn.log_softmax(x)
 
     return init_fun, apply_fun
